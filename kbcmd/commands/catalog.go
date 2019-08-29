c1ad26d57b733f1d93997efc64281cdc3442d647
Prem Ramanathan <premram@google.com>
2019-08-29 15:46:26 -0700

    Add catalog get command
    Change-Id: Ibe6a4ba1b9d03942f2eec0930392341591a86cb9

--- kbcmd/commands/catalog.go
+++ kbcmd/commands/catalog.go
@@ -4,11 +4,42 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/killbill/kbcli/kbclient/catalog"
 	"github.com/killbill/kbcli/kbcmd/cmdlib"
 	"github.com/urfave/cli"
 )
+
+func getCatalog(ctx context.Context, o *cmdlib.Options) error {
+	if len(o.Args) > 1 {
+		return cmdlib.ErrorInvalidArgs
+	}
+	var outputFile string
+
+	if len(o.Args) == 1 {
+		outputFile = o.Args[0]
+		_, err := os.Stat(outputFile)
+		if err == nil || !os.IsNotExist(err) {
+			return fmt.Errorf("output file already exists %v", outputFile)
+		}
+	}
+
+	res, err := o.Client().Catalog.GetCatalogXML(ctx, &catalog.GetCatalogXMLParams{})
+	if err != nil {
+		return err
+	}
+
+	if outputFile != "" {
+		if err = ioutil.WriteFile(outputFile, []byte(res.Payload), 0644); err != nil {
+			return fmt.Errorf("unable to write output: %v", err)
+		}
+		o.Outputln("catalog successfully written to %v", outputFile)
+	} else {
+		o.Outputln("%s", res.Payload)
+	}
+	return nil
+}
 
 func uploadCatalog(ctx context.Context, o *cmdlib.Options) error {
 	if len(o.Args) != 1 {
@@ -40,9 +71,29 @@ func registerCatalogCommands(r *cmdlib.App) {
 	}, nil)
 
 	r.Register("catalog", cli.Command{
-		Name:        "upload",
-		Usage:       "upload given catalog",
-		ArgsUsage:   "CATALOG_FILE",
+		Name:  "get",
+		Usage: "get catalog for tenant",
+		ArgsUsage: `[OUTPUT_FILE]
+		
+		For ex.,
+		
+		# To write the catalog to a file
+		kbcmd catalog upload /tmp/catalog.json
+		
+		# To write to stdout
+		kbcmd catalog upload
+`,
+		Description: "Retrieves the catalog and stores in the file",
+	}, getCatalog)
+
+	r.Register("catalog", cli.Command{
+		Name:  "upload",
+		Usage: "upload given catalog",
+		ArgsUsage: `CATALOG_FILE
+		
+For ex.,
+		kbcmd catalog upload /tmp/catalog.json
+`,
 		Description: "Uploads the catalog",
 	}, uploadCatalog)
 }
